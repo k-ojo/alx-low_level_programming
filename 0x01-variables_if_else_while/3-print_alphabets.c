90e6eeea1c38512841c8a5aa8144c5a47ae207df
Kojo <gideonad4@gmail.com>
2023-10-06 09:47:48 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- a program to outpt alphabets
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+        char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; i < 26; i++)
+        {
+                putchar(a[i]);
+        }
+
+        putchar('\n');
+        return (0);
+}
+
