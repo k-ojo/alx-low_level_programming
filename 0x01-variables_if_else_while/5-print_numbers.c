23ea339df473babe02f816dfca41e5488107d8cc
Kojo <gideonad4@gmail.com>
2023-10-06 10:32:52 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- a program to outup the base 10 digits
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i > 10; i++)
+	{
+		printf("%i", i);
+	}
+
+	putchar('\n');
+	return (0);
+}
