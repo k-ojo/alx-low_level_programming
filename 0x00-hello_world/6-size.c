825c0d7b6124b4fbfc7c8728739b26e4ecf1b7b9
Kojo <gideonad4@gmail.com>
2023-10-05 23:48:09 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main- this a program that prints the sizes of various datatypes
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)", sizeof(char));
+	printf("Size of an integer: %ld byte(s)", sizeof(int));
+	printf("Size of a long int: %ld byte(s)", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
+	return (0);
+}
