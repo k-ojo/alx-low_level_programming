1f3a070b70a6d7f237f99e6edf3b095cc1c5f2a2
Kojo <gideonad4@gmail.com>
2023-10-06 11:13:32 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- a program to output lower alphabets
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar(a[26 - i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
