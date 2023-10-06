a180a759847391662d757cc34667420fea4ebf49
Kojo <gideonad4@gmail.com>
2023-10-06 09:33:26 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- a program to output lower alphabets
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 26; i++)
+	{
+			putchar(a[i]);
+
+	}
+
+	putchar('\n');
+	return (0);
+}
