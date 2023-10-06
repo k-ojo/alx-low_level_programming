3d27c90f7fb7082c1dbed545ebc4809d7960509c
Kojo <gideonad4@gmail.com>
2023-10-06 00:44:36 -0700

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main-program to output to std error
+ *
+ * Return: 1 ass error value
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+	return (1);
+}
