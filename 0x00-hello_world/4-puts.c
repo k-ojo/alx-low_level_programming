e4be12d8b5a889f5612a5354fe173b78e6a626fb
Kojo <gideonad4@gmail.com>
2023-10-05 22:39:32 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
 
 int main(void)
 {
-	char* str="\"Programming is like building a multilingual puzzle";
+	char* str = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
-}	
+}
