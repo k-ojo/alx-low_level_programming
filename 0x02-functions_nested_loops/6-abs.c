aab82ef0504fb13294f66aa4f0a29acc8628b1bc
Kojo <gideonad4@gmail.com>
2023-10-12 00:54:29 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs: computes absolute of a number
+ * @n: the input number
+ *
+ * Return: the absolute of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (0);
+}
