6336926a0084cd023de95db9763f07ce443f1917
Kojo <gideonad4@gmail.com>
2023-10-12 05:01:14 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table- multiplication table
+ *
+ */
+void times_table(void)
+{
+	int n, i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if ((n * i) >= 10)
+			{
+				_putchar((n * i) / 10 + '0');
+				_putchar((n * i) % 10 + '0');
+				if (i != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				continue;
+			}
+
+			_putchar(n * i + '0');
+			if(i != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
