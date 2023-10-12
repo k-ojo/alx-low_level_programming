bb3b483eb1d366b1d120024f8e3e6cf9148fcd45
Kojo <gideonad4@gmail.com>
2023-10-11 22:06:48 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet- prints english alphabets
+ *
+ * Return: return 0 if success
+ */
+int print_alphabet(void)
+{
+        char i;
+
+        for(i = 'a'; i <= 'z'; i++)
+        {
+                _putchar(i);
+        }
+
+        _putchar('\n');
+        return (0);
+}
+
