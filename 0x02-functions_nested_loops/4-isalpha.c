694f216f0305ba46eb69ab4fd03862774a418c6e
Kojo <gideonad4@gmail.com>
2023-10-12 00:22:21 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,4 +6,22 @@
  *
  * Return: 1 if alphabet
  * 0 if not alphabet
- * /
+ */
+int _isalpha(int c)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			return (1);
+	}
+
+	for (i == 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			return (1)
+	}
+	return (0);
+}
+
