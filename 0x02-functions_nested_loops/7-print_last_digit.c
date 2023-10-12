5a987e0d4294cefc0c7469400fa40f2cf608aea4
Kojo <gideonad4@gmail.com>
2023-10-12 01:13:17 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * print_last_digit- prints last digit
+ * @n: input number to work on
+ *
+ * Return: the last digit on n
+ */
+int print_last_digit(int n)
+{
+	_putchar(n % 10 + '0');
+	return (n % 10);
+}
