b9607ec9a72157b4f5c51d4e5d7374ff9b043613
Kojo <gideonad4@gmail.com>
2023-10-12 05:40:03 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#inlcude "main.h"
+
+/**
+ * add- adds two integers
+ * @a: first number
+ * @b: second number to be added
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
