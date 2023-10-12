80f6768d638cb7fb7e3423adb986ffcf1912e817
Kojo <gideonad4@gmail.com>
2023-10-11 22:55:12 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**print_alphabet_x10- prints alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i,z;
+
+	for (i = 10; i > 0; i--)
+	{
+		for ( z = 'a'; z < 'z'; z++)
+		{
+			_putchar(z);
+		}
+		_putchar('\n');
+
+	}
+}
