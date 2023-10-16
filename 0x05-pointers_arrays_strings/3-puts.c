e189a957a1fc87be0d8f81aa4a7a941e5560369e
Kojo <gideonad4@gmail.com>
2023-10-16 16:08:12 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_puts- prints string to stdout
+ * @str: input string
+ *
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
