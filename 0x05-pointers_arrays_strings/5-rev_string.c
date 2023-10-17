304f1b141596301fc9038c58f9586d22c1647923
Kojo <gideonad4@gmail.com>
2023-10-16 17:06:59 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,15 @@
 void rev_string(char *s)
 {
 	int i = 0;
-	char temp[];
 
 	while (s[i] != '\0')
 	{
 		count += 1;
 		i++;
 	}
+
+	char temp[count];
+
 	for (i = count - 1; i > 0; i--)
 	{
 		temp[i] = s[i];
