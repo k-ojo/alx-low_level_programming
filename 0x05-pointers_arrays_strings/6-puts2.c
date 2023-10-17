61d5ad4710d74410478dfef55a29b2b9121787a3
Kojo <gideonad4@gmail.com>
2023-10-16 18:18:32 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_puts2- prints string to stdout
+ * @str: input string
+ *
+ */
+void _puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
