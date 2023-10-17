8a20f00fe7471672b4ad737a56475b5d9064d27c
Kojo <gideonad4@gmail.com>
2023-10-16 18:57:16 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		count += 1;
 		i++;
 	}
-	for (i = count - (count / 2) - 1; i < count - 1; i++)
+	for (i = count - (count / 2); i < count - 1; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
