8cdaa846452dca3d465365b32fcfd7469ba4f3f1
Kojo <gideonad4@gmail.com>
2023-10-19 05:05:21 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	int tmp[];
-	count = 0;
 	for (i = 0; i >= 0; i++)
 	{
 		if(dest == '\0')
 			break;
-		temp[i] = dest[i];
 	}
 	for (j = 0; j >= 0; j++)
 	{
 		if(src[j] == '\0')
 			break;
-		temp[i] = src[j];
+		dest[i] = src[j];
 		i++;
 	}
-	temp[i] = '\0';
-	return temp;
+	dest[i] = '\0';
+	return dest;
 }
