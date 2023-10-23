b22235a9d6ea7a04cd2019e747fed6e9cd1e7915
Kojo <gideonad4@gmail.com>
2023-10-23 07:49:10 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,20 @@
-#include "main.c"
+#include "main.h"
+
+/**
+ * _memcpy- copies from src to dest
+ * @dest: destination
+ * @src: source
+ * @n: number of times
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest);
+}
