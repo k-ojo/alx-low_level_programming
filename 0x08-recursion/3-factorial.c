be37d8a83993e30c4d35e3e2116bc889239892ec
Kojo <gideonad4@gmail.com>
2023-10-25 05:06:49 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+ * factorial- computes factorial
+ *
+ * @n: input integer
+ * Return: the factorial of n
+ */
+int factorial(int n)
+{
+	if (n <= 0)
+		return (1);
+	return (factorial(n - 1));
+}
