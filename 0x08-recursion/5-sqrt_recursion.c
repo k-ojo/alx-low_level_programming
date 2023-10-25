48f8d26ffa3c7169655da474b322befe8e14d318
Kojo <gideonad4@gmail.com>
2023-10-25 06:46:12 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * my_sqrt_recursion- compute squareroot
+ * @n: input integer
+ * @b: counter
+ *
+ * Return: square-root of n
+ */
+int my_sqrt_recursion(int n, b)
+{
+	if (n - b == 0)
+		return ((b / 2) + 1);
+	if (n - b < 0)
+		return (-1);
+	my_sqrt_recursion(n - b, b + 2);
+}
+
+/**
+ * _sqrt_recursion- compute squareroot
+ * @n: input integer
+ *
+ * Return: square-root of n
+ */
+int _sqrt_recursion(int n)
+{
+	my_sqrt_recursion(n, 1);
+}
