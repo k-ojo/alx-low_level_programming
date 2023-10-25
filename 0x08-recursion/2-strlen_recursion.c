58ea3e01d35c74f66866941c68c5ab1323e1ab4d
Kojo <gideonad4@gmail.com>
2023-10-25 04:43:43 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion- prints string in reverse
+ * @s: input string
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	return (1 + _strlen_recursion);
+}
