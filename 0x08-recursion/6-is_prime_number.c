37e48213bc107dac88eb46fbadfb88384328eede
Kojo <gideonad4@gmail.com>
2023-10-25 22:28:29 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * my_is_prime_number- test for primality
+ * @n: input integer
+ *@i: iterator
+ * Return: 1 if prime
+ * 0 if not
+ */
+int my_is_prime_number(int n, int i)
+{
+	if (n == 2 || n == 3)
+		return (1);
+	if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+		return (0);
+	if (n % i == 0 || n % (i + 2) == 0)
+	{
+		if ((i * i) >= n)
+			return (0);
+	}
+	my_is_prime_number(n, i + 6);
+	return (1);
+}
+
+/**
+ * is_prime_number- test for primality
+ * @n: input integer
+ *
+ * Return: 1 if prime
+ * 0 if not
+ */
+int is_prime_number(int n)
+{
+	return (my_is_prime_number(n, 5));
+}
