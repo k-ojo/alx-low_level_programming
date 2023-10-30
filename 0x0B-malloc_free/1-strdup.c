12d6355b5a8f6a13f2284c336b102cb5c3ce9fe2
Kojo <gideonad4@gmail.com>
2023-10-30 15:18:13 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen- computes length of string
+ * @str : input string
+ *
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+	int count = 0;
+
+	while (*(str + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * _strdup- copies input string
+ * @str : input string
+ *
+ * Return: pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i = 0;
+	int n = _strlen(str) + 1;
+
+	ptr = malloc(sizeof(char) * (n));
+
+	while (*(str + i) != '\0')
+	{
+		*(ptr + i) = *(str + i);
+		i++;
+	}
+
+	*(ptr + i) = *(str + i);
+	return (ptr);
+}
