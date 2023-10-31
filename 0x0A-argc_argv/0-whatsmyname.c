ca2f22591e7a3011f1452cb47b23a340213be99d
Kojo <gideonad4@gmail.com>
2023-10-31 16:03:31 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  *
  * Return: 0 on success
  */
-int main(__attribute__((unsed)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
