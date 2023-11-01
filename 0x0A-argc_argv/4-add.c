70f7c658136b6352d6ad15d7f784764d89c4dfa8
Kojo <gideonad4@gmail.com>
2023-10-31 18:40:01 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("%i\n", 0);
-		return (1);
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 		if (atoi(argv[i]) == 0)
 		{
 			printf("Error\n");
-			return (0);
+			return (1);
 		}
 		sum += atoi(argv[i]);
 	}
