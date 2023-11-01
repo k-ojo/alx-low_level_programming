2c4139e5ebbc19a63b21cc8906f193eda5c7efe7
Kojo <gideonad4@gmail.com>
2023-10-31 17:37:38 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main-prints agument to program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc);
+	return (0);
+}
