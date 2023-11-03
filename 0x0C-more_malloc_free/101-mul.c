213d09ee2dba546e16330e6641c1c0a47748cca3
Kojo <gideonad4@gmail.com>
2023-11-03 15:46:45 -0700

    task uncompleted

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
