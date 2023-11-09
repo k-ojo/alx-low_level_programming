fe9d7735a5dbd8b9d96ed528fd31f0d209023dcf
Kojo <gideonad4@gmail.com>
2023-11-08 17:22:44 -0800

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,11 +9,11 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul},
-		{"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+		{"/", op_div}, {"%", op_mod}, {0, 0}};
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (*ops[i].op == 0)
 	{
 		if (*s == *ops[i].op)
 			return (ops[i].f);
