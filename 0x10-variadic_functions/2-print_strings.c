807af25a0405ed450aabc8b7d65b3c8a129eceaa
Kojo <gideonad4@gmail.com>
2023-11-10 14:58:30 -0800

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings-print n input numbers
+ *
+ * @n:number of input inputs
+ * @separator: separator
+ *
+ * Return: sum of n inputs
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(ap, char *));
+		if (separator && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
