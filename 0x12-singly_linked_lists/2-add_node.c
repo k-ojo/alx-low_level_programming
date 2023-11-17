a2617c884e6382f1f690c6b4143c9af5b0714cec
Kojo <gideonad4@gmail.com>
2023-11-16 18:31:42 -0800

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_node- adds new node to linked list
+ *
+ * @head: a pointer to the head of the linked list
+ * @str: name of node
+ *
+ * Return: address of new node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
