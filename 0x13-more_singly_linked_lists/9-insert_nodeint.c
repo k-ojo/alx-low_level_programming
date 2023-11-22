ec1127bda309d46f8cfaf74d3780473b759bb7ab
Kojo <gideonad4@gmail.com>
2023-11-21 19:18:31 -0800

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,64 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index- inserts new element at n linked list
- * @head: input list
- * @idx: index
- * @n: input integer
+ * insert_nodeint_at_index - returns the nth node of a linked list
+ * @head: pointer to the head of the list
+ * @idx: index of the node to be added
+ * @n: content of the new node
  *
- * Return: new datum
+ * Return: the address of the node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
-	listint_t *new, *prev, *h;
+	listint_t *new_node = NULL;
+	listint_t *previous_node = NULL;
+	unsigned int i = 0;
 
-
-	if (!head)
-		return (0);
-
-	new = malloc(sizeof(listint_t));
-	new->n = n;
-	if (!new)
-		return (0);
-	if (idx == 0)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || idx > listint_len(*head))
 	{
-		new->next = *head;
-		return (new);
+		free(new_node);
+		return (NULL);
 	}
-	prev = *head;
-	h = (*head)->next;
-
-	while (*head != 0)
+	new_node->n = n;
+	new_node->next = NULL;
+	while (head != NULL)
 	{
 		if (i == idx)
 		{
-			prev->next = new;
-			new->next = h;
-			return (new);
+			if (i == 0)
+			{
+				new_node->next = *head;
+				*head = new_node;
+				return (new_node);
+			}
+			new_node->next = previous_node->next;
+			previous_node->next = new_node;
+			return (new_node);
 		}
-		prev = h;
-		h = (h)->next;
+		else if ((i + 1) == idx)
+			previous_node = *head;
+		head = &((*head)->next);
 		i++;
 	}
-	free(new);
-	return (0);
+	return (NULL);
+}
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
 }
