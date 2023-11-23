0020e5b79ffa48826109148c95cafd38a0049cbb
Kojo <gideonad4@gmail.com>
2023-11-23 10:00:22 -0800

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint- converts bits to int
+ *
+ * b:string of bits
+ *
+ * Return: converted number or 0 if fails
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0, i = 0;
+	if (b)
+	{
+		while (b[i] != '\0')
+		{
+			if (!(b[i] == 48 || b[i] == 49))
+				return (0);
+			result = result * 2 + (b[i] - '0');
+			i++;
+		}
+		return (result);
+	}
+	return (0);
+}
+
+
