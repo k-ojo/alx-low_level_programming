0d3992c87ee9a758e0d2ee1575ea73f87bb9c5b8
Kojo <gideonad4@gmail.com>
2023-11-25 03:01:10 -0800

    task 1

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * get_endianess- find the endianess of a computer
+ * get_endianness- find the endianness of a computer
  *
  * Return: architecture endianness
  */
 int get_endianness(void)
 {
 	short int holder = 1;
+
 	if (*(char *)&holder == 1)
 		return (1);
 	return (0);
