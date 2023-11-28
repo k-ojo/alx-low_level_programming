a075719103a69386e6af6e67b1d982ae459f858a
Kojo <gideonad4@gmail.com>
2023-11-27 19:43:42 -0800

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,8 +3,8 @@
 /**
  * create_file- creates a file
  * @filename: the name of file to be created
+ * @text_content: content to be added to file
  *
- * @text-content: content to be added to file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int file;
 	int n;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file != -1)
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * append_text_to_file- adds content to end of file
+ *
+ * @filename: name of file to be edited
+ * @text_content: content to be added
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, status;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		return (1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	status = write(file, text_content, strlen(text_content));
+	close(file);
+	if (status != -1)
+		return (1);
+	return (-1);
+}
