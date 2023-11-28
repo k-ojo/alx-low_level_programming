0f2d07b4e299cae87a24450610e9a54df61b2e00
Kojo <gideonad4@gmail.com>
2023-11-27 22:45:19 -0800

    final tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,6 +5,8 @@
  *
  * @filename: name of file to be edited
  * @text_content: content to be added
+ *
+ * Return: 1 on success, -1 on fail
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
